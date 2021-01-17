55ef9acb64d6c4b1339f0744537c6766c4aab92a
Cristian Bicheru <xxthegoldenbiscuitxx@gmail.com>
2021-01-16 23:18:26 -0500

    sync

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from PIL import Image
 import time
 server = Flask(__name__)
 from sightings import Sightings
+import predictor
 import uuid
 import json
 
 sightings = Sightings()
 
-def inference(img):
-    #save ..._overlay.png
-    return 0, ((0,0),(0,0))
-
 @server.route("/still-data", methods = ["POST"])
 def load_still():
+    rtime = time.time()
     data = request.files.get('still')
     img = Image.open(data.stream)
     sid = str(uuid.uuid1().int)
     img.save("stills/"+sid+".png")
-    num_geese, bbox = inference(img)
     jsondata = json.loads(request.form['json'])
-    sightings.add(sid, time.time(), jsondata["location"], jsondata["telemetry"], num_geese, bbox)
+    num_geese, bbox = predictor.infer(sid)
+    sightings.add(sid, rtime, jsondata["location"], jsondata["telemetry"], num_geese, bbox)
     return "Success", 200
 
 @server.route("/sightings", methods = ["GET"])
