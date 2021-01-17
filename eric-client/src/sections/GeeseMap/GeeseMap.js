8bd67b881b4ae60922b161b9f2d1a55db5f461f8
Kevin <kevingao2003@gmail.com>
2021-01-17 01:04:56 -0500

    revert changes back

---
+++ eric-client/src/sections/GeeseMap/GeeseMap.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, ZoomControl, useMap } from 'react-leaflet';
+import "./GeeseMap.css";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import gooseImg from "./images/goose.png";
+
+function ChangeView({ center }) {
+    const map = useMap();
+    map.setView(center);
+    return null;
+}
+
+class GeeseMap extends React.Component {
+    state = {
+        center: [43.4723, -80.5449]
+    }
+    
+    render() {
+        return (
+            <MapContainer useFlyTo attributionControl={false} center={this.state.center} zoom={16} zoomControl={false}>
+                <ChangeView center={this.state.center}/> 
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    {this.props.geese && this.props.geese.map(goose => {
+                        const geeseIcon = new L.Icon({
+                            iconUrl: gooseImg,
+                            iconRetinaUrl: gooseImg,
+                            iconSize: [50,50],
+                        })
+
+                        return <Marker position={[goose.lat, goose.lng]} icon={geeseIcon} key={goose.date} eventHandlers={{click: (e) => {
+                                    console.log("Gottem");
+                                    this.setState({center: [goose.lat, goose.lng]});
+                                    this.props.toggleDrawer(true);
+                                    this.props.changeCurrentGoose(goose);
+                                }
+                            }}/>
+                    })}
+                <ZoomControl position="topright"/>
+            </MapContainer>
+        );
+    }
+}
+
+export default GeeseMap;
