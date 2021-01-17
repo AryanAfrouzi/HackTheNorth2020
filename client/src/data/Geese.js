0103d906a5444524b2a2dbdefc96790eb3147888
Kevin <kevingao2003@gmail.com>
2021-01-17 03:43:03 -0500

    Interface skeleton complete

---
+++ client/src/data/Geese.js
@@ -0,0 +1,55 @@
+const Geese = {
+  data: [
+    {
+      lat: 43.4723,
+      lng: -80.5449,
+      date: "01/16/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+    {
+      lat: 43.4727,
+      lng: -80.5439,
+      date: "01/10/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+    {
+      lat: 43.4728,
+      lng: -80.5489,
+      date: "01/16/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+    {
+      lat: 43.472,
+      lng: -80.5339,
+      date: "01/10/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+    {
+      lat: 43.4763,
+      lng: -80.5479,
+      date: "01/16/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+    {
+      lat: 43.4721,
+      lng: -80.5431,
+      date: "01/10/2021",
+      number: 1,
+      image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+    },
+  ],
+  currentGoose: {
+    lat: 43.4727,
+    lng: -80.5439,
+    date: "01/10/2021",
+    image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+  },
+  drawerOpen: false,
+};
+
+export default Geese;
---
+++ client/src/data/sightings.js
@@ -0,0 +1,21 @@
+const SightingsData = {
+  bbox: [
+    [0, 0],
+    [0, 0],
+  ],
+  geesestimate: 0,
+  id: "184101317400033484462251740198923665410",
+  location: [1, 2],
+  telemetry: "N/A",
+  time: 1610843575.592912,
+};
+
+const ClassicData = {
+  lat: 43.4723,
+  lng: -80.5449,
+  date: "01/16/2021",
+  number: 1,
+  image: "https://i.ytimg.com/vi/JsGz-wQpEkY/maxresdefault.jpg",
+};
+
+const TransformedData = {};
